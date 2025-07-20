e21f3f0795febbe7c5d63c07cd988dcd1e7033a0
shifaul-expressitbd <shifaul@expressitbd.net>
2025-07-21 03:33:44 +0600

    modified:   src/config/constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,5 +3,3 @@ export const DEPLOY_BASE_PATH = '/var/www';
 export const PM2_PATH = '/usr/bin/pm2';
 export const NGINX_SITES_AVAILABLE = '/etc/nginx/sites-available';
 export const NGINX_SITES_ENABLED = '/etc/nginx/sites-enabled';
-export const GIT_CLONE_BASE = 'https://github.com/khanshifaul';
-export const THEMES_REPO = 'themes'; 
