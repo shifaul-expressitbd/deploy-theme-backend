27945f1d510a1cc2f17fd2c92a7ce9a2308acd5a
shifaul-expressitbd <shifaul@expressitbd.net>
2025-07-22 02:21:11 +0600

    update

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 // Constants for deployment automation
 export const DEPLOY_BASE_PATH = '/var/www';
-export const PM2_PATH = '/usr/bin/pm2';
+export const PM2_PATH = '/root/.nvm/versions/node/v22.15.0/bin/pm2';
 export const NGINX_SITES_AVAILABLE = '/etc/nginx/sites-available';
 export const NGINX_SITES_ENABLED = '/etc/nginx/sites-enabled';
